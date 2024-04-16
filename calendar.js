f09b5dcdfc5e043cacef48ac28a2abf9fe2f2144
d-geun <ehdeha@gmail.com>
2024-04-16 23:08:16 +0900

    달력 레이아웃 완성

---
+++ calendar.js
@@ -0,0 +1,5 @@
+// html 안의 요소에 대해 접근
+const currentMonth = document.querySelector('.currentMonth');
+const calendarDates = document.querySelector('.calendarDates');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
